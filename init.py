4c6f8431b8df1773ae1d6fa26f9bc7b88eb91491
borise <yemchenko.b@gmail.com>
2023-07-23 20:54:01 +0600

    pytorch to classify images

---
+++ init.py
@@ -0,0 +1,40 @@
+import torch, torchvision
+from torchvision import datasets, models, transforms
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+import time
+from torchsummary import summary
+
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+
+
+# Applying Transforms to the Data
+image_transforms = { 
+    'train': transforms.Compose([
+        transforms.RandomResizedCrop(size=256, scale=(0.8, 1.0)),
+        transforms.RandomRotation(degrees=15),
+        transforms.RandomHorizontalFlip(),
+        transforms.CenterCrop(size=224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406],
+                            [0.229, 0.224, 0.225])
+    ]),
+    'valid': transforms.Compose([
+        transforms.Resize(size=256),
+        transforms.CenterCrop(size=224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406],
+                            [0.229, 0.224, 0.225])
+    ]),
+    'test': transforms.Compose([
+        transforms.Resize(size=256),
+        transforms.CenterCrop(size=224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406],
+                            [0.229, 0.224, 0.225])
+    ])
+}
